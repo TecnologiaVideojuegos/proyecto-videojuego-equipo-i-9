afac930fa57c5f743f32177c48c968ab3909c3c6
sadgams12 <37366174+sadgams12@users.noreply.github.com>
2018-04-27 16:35:22 +0200

    Mapa v.0.3beta
    Hay que coger también los .png o .gif de la carpeta de sprites. Son los siguientes:
    -animated_torch.gif (no sé si para que se muevan hay que usar el .gif o el .png)
    -forrestuo.png
    -teselas__Tiles.png

---
+++ infierno.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="infierno" tilewidth="32" tileheight="32" tilecount="36" columns="6">
+ <image source="Sprites/forrestup.png" trans="ffffff" width="192" height="192"/>
+</tileset>
