c57a94833f0af37e3e55bc26f9f881fcc9113327
sadgams12 <37366174+sadgams12@users.noreply.github.com>
2018-05-04 15:35:22 +0200

    MAPA

---
+++ Mapas/patron.tsx.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="patron.tsx" tilewidth="32" tileheight="32" tilecount="900" columns="30">
+ <image source="../Sprites/hyptosis_tile-art-batch-3.png" trans="ffffff" width="960" height="960"/>
+</tileset>
