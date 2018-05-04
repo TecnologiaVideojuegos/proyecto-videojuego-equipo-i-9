8feb32d9d8f7b123f6c84612983f094688913758
sadgams12 <37366174+sadgams12@users.noreply.github.com>
2018-05-04 10:53:20 +0200

    Carpetas Mapas

--- Mapas/materiales/tsx/teselastiles.tsx
+++ Mapas/materiales/tsx/teselastiles.tsx
@@ -1,4 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="teselastiles" tilewidth="32" tileheight="32" tilecount="360" columns="8">
+ <grid orientation="orthogonal" width="1400" height="900"/>
  <image source="Sprites/teselas___Tiles.png" trans="ffffff" width="256" height="1450"/>
 </tileset>
