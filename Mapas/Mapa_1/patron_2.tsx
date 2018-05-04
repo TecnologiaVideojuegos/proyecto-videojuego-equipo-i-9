27ae1c88fd060adc69b99ed8cff480652cf7aace
sadgams12 <37366174+sadgams12@users.noreply.github.com>
2018-05-04 16:22:57 +0200

    camino básico

---
+++ Mapas/Mapa_1/patron_2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="classical_temple_tiles" tilewidth="32" tileheight="32" tilecount="768" columns="32">
+ <image source="../../Sprites/classical_temple_tiles.png" trans="ffffff" width="1024" height="768"/>
+</tileset>
