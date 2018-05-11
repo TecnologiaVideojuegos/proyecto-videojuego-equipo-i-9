1c79df54a3fe38b702c09fe647195213a8378ba6
Antoniogb <a.gomezb@edu.uah.es>
2018-05-11 16:08:32 +0200

    juego

---
+++ Slick2d_1/src/Aa_juego/SubditoExplorador.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aa_juego;
+
+/**
+ *
+ * @author dante
+ */
+public class SubditoExplorador extends Subdito{
+    
+    
+    public SubditoExplorador (double xPos, double yPos){
+        
+        super (xPos, yPos, health, speed, armor, name, type.Pathfinder);
+    }
+    private static String name = "Pathfinder";
+    private static double health = 100;
+    private static double speed = 1.1;
+    private static double armor = 0;
+    
+}
---
+++ Slick2d_1/src/Aa_juego/SubditoSoldado.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aa_juego;
+
+/**
+ *
+ * @author dante
+ */
+public class SubditoSoldado extends Subdito{
+    
+    
+    public SubditoSoldado (double xPos, double yPos){      
+        super (xPos, yPos, health, speed, armor, name, type.Soldier);
+    }
+    private static String name = "Soldier";
+    private static double health = 150;
+    private static double speed = 0.75;
+    private static double armor = 10;
+    
+}
